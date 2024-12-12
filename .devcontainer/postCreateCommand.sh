b23739b43efa8422d9e18e82c4b13e4742b02b71
Marius Rugan <mariusrugan@gmail.com>
2024-12-12 20:23:20 +0100

    devcontainer config

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,14 @@
 set -e
 set -o noglob
 
-apt-get update -y && apt-get install -y --no-install-recommends openssl ca-certificates pkg-config libssl-dev xgboost libclang-dev
+apt-get update -y && apt-get install -y --no-install-recommends \
+    openssl ca-certificates \
+    pkg-config libssl-dev \
+    xgboost libclang-dev \
+    direnv fzf \
+    gettext \
+    jq \
+    python3 python3-pip
+
 
 export LIBCLANG_PATH=/usr/lib/llvm-11/lib
